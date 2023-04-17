d5da1b049c53d50a7b5478d42e9a67b183ee8de2
wan-mureithi <estherdee27@gmail.com>
2023-04-18 00:25:06 +0300

    layout and static images

---
+++ src/components/layout.tsx
@@ -0,0 +1,39 @@
+import { Link } from 'gatsby'
+import React, { ReactNode } from 'react'
+import {container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText} from './layout.module.css'
+
+interface LayoutProps  {
+    pageTitle: string;
+    children: ReactNode;
+}
+const Layout: React.FC<LayoutProps> = ({pageTitle,children}) => {
+  return (
+    <div className={container}>
+          <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+
+  </div>
+  )
+}
+
+export default Layout
---
+++ src/pages/about.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { HeadFC, PageProps } from "gatsby"
+import Layout from '../components/layout'
+import { StaticImage } from "gatsby-plugin-image"
+
+const AboutPage: React.FC<PageProps> = () => {
+  return (
+    <Layout pageTitle="About Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+      alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+      src="../images/hot-2.jpg"/>
+    </Layout>
+  )
+}
+
+export default AboutPage
+
+
+export const Head: HeadFC = () => <title>About Page</title>
